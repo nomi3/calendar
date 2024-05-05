ca7a6ac24508fadb82914142dc0da4e088e9aa73
nomi3 <thisisnomi3@gmail.com>
2024-05-05 09:31:21 +0900

    背景色の表示

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -96,6 +96,7 @@
                                 </div>
                             @elseif ($meeting_end == $half_hour + 1800)
                                 <div class="meeting">
+                                    &ZeroWidthSpace;
                                 </div>
                             @endif
                         @endforeach
